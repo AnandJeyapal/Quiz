a812f22896b51458f239182788a326fd950eab82
AnandJeyapal <janand1991@gmail.com>
2023-01-31 14:30:28 +0400

    Added Confetti in EndPage.

--- src/components/EndPage.js
+++ src/components/EndPage.js
@@ -4,7 +4,12 @@ import WinnerPage from './WinnerPage'
 import LoserPage from './LoserPage'
 import PassPage from './PassPage'
 
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
 export default function EndPage(props) {
+  const { width, height } = useWindowSize()
+
   let validAnswers = props.answers.filter(ans => ans.answer === ans.correctAnswer).length;
   let winner = validAnswers >= 10;
   let pass = validAnswers >= 7 && validAnswers <10;
@@ -13,6 +18,10 @@ export default function EndPage(props) {
 
   return (
     <div>
+      <Confetti
+      width={width}
+      height={height}
+      />
       {winner && <WinnerPage name = {props.name} correctAnswers = {validAnswers} restartQuiz = {props.restartQuiz}/>}
       {pass && <PassPage  name = {props.name} correctAnswers = {validAnswers} restartQuiz = {props.restartQuiz}/>}
       {loser && <LoserPage  name = {props.name} correctAnswers = {validAnswers} restartQuiz = {props.restartQuiz}/>}
